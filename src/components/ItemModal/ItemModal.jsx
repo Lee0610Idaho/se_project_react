8023f4b5ab0a459d4b3bba17771aedf639cea0cb
Jackie Lee <jackieleeid2000@gmail.com>
2025-07-30 12:06:29 -0600

    updated delete button to show for authorized users

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,6 +1,18 @@
 import "./ItemModal.css";
+import { useContext } from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
 function ItemModal({ activeModal, onClose, card, deleteItem }) {
+  const currentUser = useContext(CurrentUserContext);
+
+  // Checking if the current user is the owner of the current clothing item
+  const isOwn = card.owner === currentUser._id;
+
+  // Creating a variable which you'll then set in `className` for the delete button
+  const itemDeleteButtonClassName = `modal__delete-button modal__delete-button_visible ${
+    isOwn ? "" : "modal__delete-button_hidden"
+  }`;
+
   const handleDeleteItem = () => {
     deleteItem(card._id);
   };
@@ -17,13 +29,15 @@ function ItemModal({ activeModal, onClose, card, deleteItem }) {
         <div className="modal__footer">
           <div className="modal__footer_row_initial-row">
             <h2 className="modal__caption">{card.name}</h2>
-            <button
-              className="modal__delete-button"
-              type="button"
-              onClick={handleDeleteItem}
-            >
-              Delete item
-            </button>
+            {isOwn && (
+              <button
+                className={itemDeleteButtonClassName}
+                type="button"
+                onClick={handleDeleteItem}
+              >
+                Delete item
+              </button>
+            )}
           </div>
           <p className="modal__weather">Weather: {card.weather}</p>
         </div>
